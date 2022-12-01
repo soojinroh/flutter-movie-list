e66d00152df925bec3219a02b4e6ede6ec617c7e
soojin roh <soojin@woowahan.com>
2022-12-01 20:07:34 +0900

    [step1] moviebloc import문 추가

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_movie_list/api/api_provider.dart';
+import 'package:flutter_movie_list/bloc/movies_bloc.dart';
 import 'package:flutter_movie_list/model/model.dart';
 import 'package:flutter_movie_list/repository/repository.dart';
 
